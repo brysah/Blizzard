06a29de89a4a2f489d9aeb67f30c7abdaca61b75
Brysah <brysahvidal@gmail.com>
2023-04-06 21:37:48 -0300

    ADD: seção hero estilizada e estruturada

--- js/all.js
+++ js/all.js
@@ -1,1 +1,1 @@
-var slide_hero=new Swiper(".slide-principal",{effect:"fade"});
+var slide_thumbnail=new Swiper(".slide-thumbnail",{slidesPerView:5,direction:"vertical",spaceBetweem:20,watchSlidesProgress:!0}),slide_hero=new Swiper(".slide-principal",{effect:"fade",thumbs:{swiper:slide_thumbnail},autoplay:{delay:4e3,disableOnInteraction:!1}});
--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,3 +1,18 @@
+var slide_thumbnail = new Swiper(".slide-thumbnail", {
+    slidesPerView: 5,
+    direction:'vertical',
+    spaceBetweem: 20,
+    watchSlidesProgress:true
+});
+
+
 var slide_hero = new Swiper(".slide-principal", {
-    effect: 'fade'
+    effect: 'fade',
+    thumbs: {
+        swiper: slide_thumbnail,
+    },
+    autoplay:{
+        delay: 4000,
+        disableOnInteraction: false
+    }
 });
